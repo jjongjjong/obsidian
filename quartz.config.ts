f34ef572356116cfc2a4f3cb18fb9c30799dc0d4
jjongjjong <jangood1122@gmail.com>
2024-03-17 22:12:39 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴JJong's Digital Garden🪴",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
